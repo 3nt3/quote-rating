041fd980a396dcddc0eafef6cddd591cbe450c40
3nt3 <gott@3nt3.de>
2022-10-21 23:50:23 +0200

    return username as well
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(async_closure)]
 #[macro_use]
 extern crate rocket;
 
@@ -6,10 +7,25 @@ use once_cell::sync::OnceCell;
 use rocket::{
     futures::TryFutureExt,
     serde::{json::Json, Serialize},
+    State,
 };
 use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use serde::Deserialize;
+
+use serenity::client::bridge::gateway::ShardManager;
+use serenity::http::Http;
+use serenity::model::event::ResumedEvent;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use serenity::{
+    async_trait, client,
+    model::prelude::{GuildId, UserId},
+};
+use tokio::runtime::Runtime;
+
 use sqlx::{postgres::PgPoolOptions, query, Pool, Postgres};
-use std::env;
+use std::fs;
+use std::{env, thread};
 
 static POOL: OnceCell<Pool<Postgres>> = OnceCell::new();
 
@@ -24,20 +40,22 @@ struct Quote {
     #[serde(with = "ts_milliseconds")]
     sent_at: chrono::DateTime<Utc>,
     avatar_url: Option<String>,
+    username: String,
     score: i64,
 }
 
 #[get("/quote")]
-async fn get_quote() -> Json<Quote> {
+async fn get_quote(client: &State<Client>) -> Json<Quote> {
     let pool = POOL.get().unwrap();
     let result = sqlx::query!("SELECT quotes.*, SUM(v.vote) AS score FROM quotes LEFT JOIN votes v on quotes.id = v.quote_id GROUP BY quotes.id ORDER BY random() LIMIT 1")
         .fetch_one(pool)
-        .map_ok(|r| {
+        .map_ok(async move |r| {
             Json(Quote {
                 id: r.id,
                 content: r.content,
                 author_id: u64::from_str_radix(&r.author_id, 10).unwrap(),
                 avatar_url: r.avatar_url,
+                username: get_username(&client, u64::from_str_radix(&r.author_id, 10).unwrap()).await.unwrap(),
                 created_at: r.created_at,
                 sent_at: r.sent_at,
                 score: r.score.unwrap_or(0)
@@ -46,11 +64,22 @@ async fn get_quote() -> Json<Quote> {
         .await
         .unwrap();
 
-    result
+    result.await
+}
+
+async fn get_username(client: &Client, user_id: u64) -> Option<String> {
+    GuildId(816943824630710272)
+        .members(&client.cache_and_http.http, Some(1), UserId(user_id))
+        .await
+        .unwrap()
+        .first()
+        .unwrap()
+        .nick
+        .clone()
 }
 
 #[post("/vote/<id>/<vote>")]
-async fn vote(id: i32, vote: i32) -> Json<Quote> {
+async fn vote(client: &State<Client>, id: i32, vote: i32) -> Json<Quote> {
     let pool = POOL.get().unwrap();
 
     let vote_cleaned = if vote > 0 {
@@ -80,12 +109,13 @@ async fn vote(id: i32, vote: i32) -> Json<Quote> {
         id
     )
     .fetch_one(pool)
-    .map_ok(|r| {
+    .map_ok(async move |r| {
         Json(Quote {
             id: r.id,
             content: r.content,
             author_id: u64::from_str_radix(&r.author_id, 10).unwrap(),
             created_at: r.created_at,
+            username: get_username(&client, u64::from_str_radix(&r.author_id, 10).unwrap()).await.unwrap(),
             avatar_url: r.avatar_url,
             sent_at: r.sent_at,
             score: r.score.unwrap_or(0),
@@ -94,12 +124,37 @@ async fn vote(id: i32, vote: i32) -> Json<Quote> {
     .await
     .unwrap();
 
-    result
+    result.await
 }
 
 #[rocket::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
+    // read configuration
+    let maybe_config = get_config();
+    if let None = maybe_config {
+        eprintln!("couldn't read config");
+        std::process::exit(1);
+    }
+    let config = maybe_config.unwrap();
+
+    // set up discord bot
+
+    let intents = GatewayIntents::non_privileged()
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILDS
+        | GatewayIntents::GUILD_MESSAGES;
+
+    let mut client = Client::builder(config.token, intents)
+        .await
+        .expect("Error creating client");
+
+    // thread::spawn(|| {
+    //     let mut rt = Runtime::new().unwrap();
+    //     rt.block_on(async move {
+    //         client.start().await;
+    //     });
+    // });
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -118,10 +173,35 @@ async fn main() -> anyhow::Result<()> {
     .to_cors()?;
 
     let _ = rocket::build()
+        .manage(client)
         .mount("/", routes![get_quote, vote])
         .attach(cors)
         .launch()
         .await?;
 
     Ok(())
+}
+
+#[derive(Deserialize)]
+struct Config {
+    token: String,
+}
+
+fn get_config() -> Option<Config> {
+    let content_result = fs::read_to_string("./config.toml");
+    if let Err(error) = content_result {
+        println!("error reading config.toml: {error}");
+        return None;
+    }
+    let content = content_result.unwrap();
+
+    match toml::from_str(&content) {
+        Ok(config) => {
+            return Some(config);
+        }
+        Err(error) => {
+            eprintln!("{}", error);
+            return None;
+        }
+    }
 }
