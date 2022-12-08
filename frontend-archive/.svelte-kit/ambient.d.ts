f6abf7f2d872c8a0688019ad9c64186a1a124c16
3nt3 <gott@3nt3.de>
2022-12-08 15:18:54 +0100

    idk
    Signed-off-by: 3nt3 <gott@3nt3.de>

---
+++ frontend-archive/.svelte-kit/ambient.d.ts
@@ -0,0 +1,358 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const SHELL: string;
+	export const LSCOLORS: string;
+	export const npm_package_devDependencies__smui_icon_button: string;
+	export const NVM_RC_VERSION: string;
+	export const COLORTERM: string;
+	export const CSF_MDTVTexturesDirectory: string;
+	export const LESS: string;
+	export const CSF_DrawPluginDefaults: string;
+	export const I3SOCK: string;
+	export const NODE: string;
+	export const npm_package_devDependencies_tslib: string;
+	export const npm_config_ignore_scripts: string;
+	export const TEXMFHOME: string;
+	export const CSF_LANGUAGE: string;
+	export const npm_package_scripts_check_watch: string;
+	export const CSF_MIGRATION_TYPES: string;
+	export const npm_package_dependencies_svelte_chartjs: string;
+	export const npm_config_argv: string;
+	export const npm_config_bin_links: string;
+	export const CSF_OCCTResourcePath: string;
+	export const KITTY_PID: string;
+	export const XCURSOR_SIZE: string;
+	export const FLUTTER_HOME: string;
+	export const CSF_STEPDefaults: string;
+	export const GPG_TTY: string;
+	export const EDITOR: string;
+	export const npm_package_devDependencies__smui_layout_grid: string;
+	export const XDG_SEAT: string;
+	export const TEXMFLOCAL: string;
+	export const PWD: string;
+	export const npm_config_save_prefix: string;
+	export const npm_package_devDependencies_vite: string;
+	export const LOGNAME: string;
+	export const XDG_SESSION_TYPE: string;
+	export const DRAWHOME: string;
+	export const npm_package_readmeFilename: string;
+	export const SYSTEMD_EXEC_PID: string;
+	export const npm_package_scripts_build: string;
+	export const _: string;
+	export const CSF_StandardLiteDefaults: string;
+	export const npm_package_devDependencies_prettier: string;
+	export const KITTY_PUBLIC_KEY: string;
+	export const FZF_DEFAULT_COMMAND: string;
+	export const npm_package_scripts_smui_theme_light: string;
+	export const MOTD_SHOWN: string;
+	export const npm_package_devDependencies__smui_button: string;
+	export const npm_package_scripts_package: string;
+	export const HOME: string;
+	export const npm_config_version_git_tag: string;
+	export const LANG: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const LS_COLORS: string;
+	export const _JAVA_AWT_WM_NONREPARENTING: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const npm_config_init_license: string;
+	export const npm_package_version: string;
+	export const SPICETIFY_INSTALL: string;
+	export const SWAYSOCK: string;
+	export const WAYLAND_DISPLAY: string;
+	export const npm_package_scripts_prepare: string;
+	export const npm_package_devDependencies__smui_card: string;
+	export const npm_config_version_commit_hooks: string;
+	export const KITTY_WINDOW_ID: string;
+	export const npm_package_devDependencies__material_theme: string;
+	export const INVOCATION_ID: string;
+	export const npm_package_devDependencies_prettier_plugin_svelte: string;
+	export const INIT_CWD: string;
+	export const CSF_ShadersDirectory: string;
+	export const CSF_EXCEPTION_PROMPT: string;
+	export const npm_package_devDependencies__material_typography: string;
+	export const CSF_XmlOcafResource: string;
+	export const npm_package_scripts_format: string;
+	export const npm_package_scripts_preview: string;
+	export const npm_lifecycle_script: string;
+	export const npm_package_description: string;
+	export const NVM_DIR: string;
+	export const CSF_SHMessage: string;
+	export const npm_config_version_tag_prefix: string;
+	export const YARN_WRAP_OUTPUT: string;
+	export const npm_package_dependencies_chart_js: string;
+	export const npm_package_devDependencies_svelte_check: string;
+	export const XDG_SESSION_CLASS: string;
+	export const ANDROID_HOME: string;
+	export const TERM: string;
+	export const TERMINFO: string;
+	export const npm_package_name: string;
+	export const ZSH: string;
+	export const npm_package_type: string;
+	export const USER: string;
+	export const npm_package_devDependencies__smui_circular_progress: string;
+	export const MARKPATH: string;
+	export const npm_package_devDependencies__mdi_js: string;
+	export const CSF_StandardDefaults: string;
+	export const CSF_IGESDefaults: string;
+	export const PYTHON_KEYRING_BACKEND: string;
+	export const DISPLAY: string;
+	export const CSF_XCAFDefaults: string;
+	export const npm_lifecycle_event: string;
+	export const SHLVL: string;
+	export const npm_config_version_git_sign: string;
+	export const NVM_CD_FLAGS: string;
+	export const npm_config_version_git_message: string;
+	export const PAGER: string;
+	export const XDG_VTNR: string;
+	export const CSF_PluginDefaults: string;
+	export const CSF_TObjMessage: string;
+	export const XDG_SESSION_ID: string;
+	export const npm_config_user_agent: string;
+	export const CASROOT: string;
+	export const npm_package_scripts_lint: string;
+	export const npm_package_devDependencies_smui_theme: string;
+	export const npm_execpath: string;
+	export const npm_package_devDependencies__sveltejs_adapter_auto: string;
+	export const npm_package_scripts_smui_theme_dark: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const npm_package_devDependencies_svelte_preprocess: string;
+	export const npm_config_strict_ssl: string;
+	export const DEBUGINFOD_URLS: string;
+	export const npm_package_scripts_dev: string;
+	export const LC_ALL: string;
+	export const CSF_XSMessage: string;
+	export const XCURSOR_THEME: string;
+	export const MMGT_CLEAR: string;
+	export const XDG_DATA_DIRS: string;
+	export const npm_package_scripts_check: string;
+	export const PATH: string;
+	export const CSF_TObjDefaults: string;
+	export const npm_package_devDependencies__smui_top_app_bar: string;
+	export const npm_package_devDependencies__sveltejs_kit: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const npm_package_license: string;
+	export const MAIL: string;
+	export const npm_config_registry: string;
+	export const npm_config_ignore_optional: string;
+	export const DRAWDEFAULT: string;
+	export const KITTY_INSTALLATION_DIR: string;
+	export const npm_node_execpath: string;
+	export const npm_config_engine_strict: string;
+	export const OLDPWD: string;
+	export const npm_config_init_version: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		SHELL: string;
+		LSCOLORS: string;
+		npm_package_devDependencies__smui_icon_button: string;
+		NVM_RC_VERSION: string;
+		COLORTERM: string;
+		CSF_MDTVTexturesDirectory: string;
+		LESS: string;
+		CSF_DrawPluginDefaults: string;
+		I3SOCK: string;
+		NODE: string;
+		npm_package_devDependencies_tslib: string;
+		npm_config_ignore_scripts: string;
+		TEXMFHOME: string;
+		CSF_LANGUAGE: string;
+		npm_package_scripts_check_watch: string;
+		CSF_MIGRATION_TYPES: string;
+		npm_package_dependencies_svelte_chartjs: string;
+		npm_config_argv: string;
+		npm_config_bin_links: string;
+		CSF_OCCTResourcePath: string;
+		KITTY_PID: string;
+		XCURSOR_SIZE: string;
+		FLUTTER_HOME: string;
+		CSF_STEPDefaults: string;
+		GPG_TTY: string;
+		EDITOR: string;
+		npm_package_devDependencies__smui_layout_grid: string;
+		XDG_SEAT: string;
+		TEXMFLOCAL: string;
+		PWD: string;
+		npm_config_save_prefix: string;
+		npm_package_devDependencies_vite: string;
+		LOGNAME: string;
+		XDG_SESSION_TYPE: string;
+		DRAWHOME: string;
+		npm_package_readmeFilename: string;
+		SYSTEMD_EXEC_PID: string;
+		npm_package_scripts_build: string;
+		_: string;
+		CSF_StandardLiteDefaults: string;
+		npm_package_devDependencies_prettier: string;
+		KITTY_PUBLIC_KEY: string;
+		FZF_DEFAULT_COMMAND: string;
+		npm_package_scripts_smui_theme_light: string;
+		MOTD_SHOWN: string;
+		npm_package_devDependencies__smui_button: string;
+		npm_package_scripts_package: string;
+		HOME: string;
+		npm_config_version_git_tag: string;
+		LANG: string;
+		npm_package_devDependencies_typescript: string;
+		LS_COLORS: string;
+		_JAVA_AWT_WM_NONREPARENTING: string;
+		XDG_CURRENT_DESKTOP: string;
+		npm_config_init_license: string;
+		npm_package_version: string;
+		SPICETIFY_INSTALL: string;
+		SWAYSOCK: string;
+		WAYLAND_DISPLAY: string;
+		npm_package_scripts_prepare: string;
+		npm_package_devDependencies__smui_card: string;
+		npm_config_version_commit_hooks: string;
+		KITTY_WINDOW_ID: string;
+		npm_package_devDependencies__material_theme: string;
+		INVOCATION_ID: string;
+		npm_package_devDependencies_prettier_plugin_svelte: string;
+		INIT_CWD: string;
+		CSF_ShadersDirectory: string;
+		CSF_EXCEPTION_PROMPT: string;
+		npm_package_devDependencies__material_typography: string;
+		CSF_XmlOcafResource: string;
+		npm_package_scripts_format: string;
+		npm_package_scripts_preview: string;
+		npm_lifecycle_script: string;
+		npm_package_description: string;
+		NVM_DIR: string;
+		CSF_SHMessage: string;
+		npm_config_version_tag_prefix: string;
+		YARN_WRAP_OUTPUT: string;
+		npm_package_dependencies_chart_js: string;
+		npm_package_devDependencies_svelte_check: string;
+		XDG_SESSION_CLASS: string;
+		ANDROID_HOME: string;
+		TERM: string;
+		TERMINFO: string;
+		npm_package_name: string;
+		ZSH: string;
+		npm_package_type: string;
+		USER: string;
+		npm_package_devDependencies__smui_circular_progress: string;
+		MARKPATH: string;
+		npm_package_devDependencies__mdi_js: string;
+		CSF_StandardDefaults: string;
+		CSF_IGESDefaults: string;
+		PYTHON_KEYRING_BACKEND: string;
+		DISPLAY: string;
+		CSF_XCAFDefaults: string;
+		npm_lifecycle_event: string;
+		SHLVL: string;
+		npm_config_version_git_sign: string;
+		NVM_CD_FLAGS: string;
+		npm_config_version_git_message: string;
+		PAGER: string;
+		XDG_VTNR: string;
+		CSF_PluginDefaults: string;
+		CSF_TObjMessage: string;
+		XDG_SESSION_ID: string;
+		npm_config_user_agent: string;
+		CASROOT: string;
+		npm_package_scripts_lint: string;
+		npm_package_devDependencies_smui_theme: string;
+		npm_execpath: string;
+		npm_package_devDependencies__sveltejs_adapter_auto: string;
+		npm_package_scripts_smui_theme_dark: string;
+		npm_package_devDependencies_svelte: string;
+		XDG_RUNTIME_DIR: string;
+		npm_package_devDependencies_svelte_preprocess: string;
+		npm_config_strict_ssl: string;
+		DEBUGINFOD_URLS: string;
+		npm_package_scripts_dev: string;
+		LC_ALL: string;
+		CSF_XSMessage: string;
+		XCURSOR_THEME: string;
+		MMGT_CLEAR: string;
+		XDG_DATA_DIRS: string;
+		npm_package_scripts_check: string;
+		PATH: string;
+		CSF_TObjDefaults: string;
+		npm_package_devDependencies__smui_top_app_bar: string;
+		npm_package_devDependencies__sveltejs_kit: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		npm_package_license: string;
+		MAIL: string;
+		npm_config_registry: string;
+		npm_config_ignore_optional: string;
+		DRAWDEFAULT: string;
+		KITTY_INSTALLATION_DIR: string;
+		npm_node_execpath: string;
+		npm_config_engine_strict: string;
+		OLDPWD: string;
+		npm_config_init_version: string;
+		[key: string]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: string]: string | undefined;
+	}
+}
